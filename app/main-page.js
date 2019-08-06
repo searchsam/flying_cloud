8d28c985b3d4dcef7d871f2d53bd27afeb5af8e8
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-08-06 16:40:36 -0600

    Get location test

--- app/main-page.js
+++ app/main-page.js
@@ -1,20 +1,35 @@
-var fetchModule = require('tns-core-modules/fetch/fetch');
+const fetchModule = require('tns-core-modules/fetch/fetch');
+const geolocation = require("nativescript-geolocation");
+const server = '192.168.0.10:5000'
 
 exports.bulkSelect = () => {
-    fetchModule.fetch("http://192.168.0.10:5000/pond/checkusb", {
-        method: "GET"
-    })
-    .then(function(response) {
-        const status = response.status;
-        if (parseInt(status) == 200) {
-            alert(response._bodyText);
-        } else {
-            alert(JSON.stringify(response));
-            // alert({title: "GET Response", message: JSON.stringify(response), okButtonText: "Close"});
+    var location = geolocation.getCurrentLocation(
+        {desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}
+    ).then(function(loc) {
+        conole.log('on function')
+        if (loc) {
+            conole.log('on if')
+            alert("Current location is: " + loc);
         }
     }, function(error) {
-        console.log(JSON.stringify(error));
+        console.log('on error')
+        alert("Error: " + error.message);
     })
+    console.log(location)
+
+    // fetchModule.fetch("http:" + server + "/pond/checkusb", {
+    //     method: "GET"
+    // })
+    // .then(function(response) {
+    //     const status = response.status;
+    //     if (parseInt(status) == 200) {
+    //         alert({title: "GET Response", message: response._bodyText, okButtonText: "Close"});
+    //     } else {
+    //         alert({title: "GET Response", message: response._bodyText, okButtonText: "Close"});
+    //     }
+    // }, function(error) {
+    //     console.log(JSON.stringify(error));
+    // })
 
     // const page = args.object
     // page.frame.navigate('secundary')
