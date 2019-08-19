024b161915da2ff5c034245e72889b8f6a6c9d02
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-08-19 09:01:23 -0600

    Metodo de georeferencia modificado para optener valores

--- app/main-page.js
+++ app/main-page.js
@@ -3,19 +3,24 @@ const geolocation = require("nativescript-geolocation");
 const server = '192.168.0.10:5000'
 
 exports.bulkSelect = () => {
+    var lat = 0;
+    var lon = 0;
     var location = geolocation.getCurrentLocation(
         {desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}
     ).then(function(loc) {
-        conole.log('on function')
+        // conole.log('on function')
         if (loc) {
-            conole.log('on if')
-            alert("Current location is: " + loc);
+            // conole.log('on if')
+            lat = loc.latitude;
+            lon = loc.longitude;
+            alert("Current location: \n latitude=>" + loc.latitude + "\n longitude=>" + loc.longitude);
         }
     }, function(error) {
-        console.log('on error')
+        // console.log('on error')
         alert("Error: " + error.message);
     })
-    console.log(location)
+    // console.log(location)
+    alert(lat + "-" + lon);
 
     // fetchModule.fetch("http:" + server + "/pond/checkusb", {
     //     method: "GET"
