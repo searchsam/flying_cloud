4e2bccb91d1b69b8d0583c336c31519f27e755ca
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2019-08-19 14:25:16 -0600

    Geo referencia y cambio de estilo

--- app/main-page.js
+++ app/main-page.js
@@ -3,39 +3,27 @@ const geolocation = require("nativescript-geolocation");
 const server = '192.168.0.10:5000'
 
 exports.bulkSelect = () => {
-    var lat = 0;
-    var lon = 0;
-    var location = geolocation.getCurrentLocation(
-        {desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}
-    ).then(function(loc) {
-        // conole.log('on function')
+    geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000})
+    .then( loc => {
         if (loc) {
-            // conole.log('on if')
-            lat = loc.latitude;
-            lon = loc.longitude;
-            alert("Current location: \n latitude=>" + loc.latitude + "\n longitude=>" + loc.longitude);
+            alert("Current location: \n latitude=>" + loc.latitude + "\n longitude=>" + loc.longitude)
+            // fetchModule.fetch("http:" + server + "/pond/checkusb", {
+            //     method: "GET"
+            // })
+            // .then( response => {
+            //     const status = response.status;
+            //     if (parseInt(status) == 200) {
+            //         alert({title: "GET Response", message: response._bodyText, okButtonText: "Close"});
+            //     } else {
+            //         alert({title: "GET Response", message: response._bodyText, okButtonText: "Close"});
+            //     }
+            // })
+            // .catch( error => {
+            //     console.log(JSON.stringify(error));
+            // })
         }
-    }, function(error) {
-        // console.log('on error')
-        alert("Error: " + error.message);
     })
-    // console.log(location)
-    alert(lat + "-" + lon);
-
-    // fetchModule.fetch("http:" + server + "/pond/checkusb", {
-    //     method: "GET"
-    // })
-    // .then(function(response) {
-    //     const status = response.status;
-    //     if (parseInt(status) == 200) {
-    //         alert({title: "GET Response", message: response._bodyText, okButtonText: "Close"});
-    //     } else {
-    //         alert({title: "GET Response", message: response._bodyText, okButtonText: "Close"});
-    //     }
-    // }, function(error) {
-    //     console.log(JSON.stringify(error));
-    // })
-
-    // const page = args.object
-    // page.frame.navigate('secundary')
+    .catch( error => {
+        alert("Error: " + error.message)
+    })
 }
